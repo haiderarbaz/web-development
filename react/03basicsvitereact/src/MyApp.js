e6a5693b0630b4a1a7f96ae2412864691b5bc54e
Arbaz Haider <admin@arbazhaider.com>
2024-12-06 01:36:29 +0530

    add txt file to react folder

---
+++ react/03basicsvitereact/src/MyApp.js
@@ -0,0 +1,5 @@
+function MyApp() {
+  return <h2>Welcome to React World ❤️</h2>;
+}
+
+export default MyApp;
--- react/03basicsvitereact/src/index.js
+++ react/03basicsvitereact/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import MyApp from "./MyApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <MyApp />
   </React.StrictMode>
 );
