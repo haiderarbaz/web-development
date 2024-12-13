ed9ca3f3b8b83a8b1d4e83ed2fc49beb2ad8460b
Arbaz Haider <admin@arbazhaider.com>
2024-12-13 23:05:27 +0530

    push the code to logicaloperators file

--- JavaScript/JSBasics1/logicaloperators.js
+++ JavaScript/JSBasics1/logicaloperators.js
@@ -2,9 +2,13 @@
  * Logical Operator with Conditional Statement
  * 1. OR || :- Any single condition is true then OR will return true, if all condition is false then OR will return false.
  * 2. AND && :- All condition have to be true then AND will return true otherwise AND will return false.
- * 3. NOT !
- * 4. NULLISH COALESCING ??
+ * 3. NOT ! :- The NOT operator reverses the Boolean value of the operand. If the operand is true, it returns false, and if the operand is false, it returns true.
  *
+ *
+ */
+
+/**
+ * Note: We only use OR operator for short circuiting and we don't use AND opertor for "SHORT CIRCUITING".
  */
 
 //Exercise Example:
@@ -51,6 +55,31 @@ if (background === "PCM") {
   console.log("Invalid entry. Please enter either 'PCM' or 'PCB'.");
 }
 
+//Truthy & Falsy
+
+//And will always search for the first falsy value, it doesn't search for truthy value and it will return the first falsy value and if there is no falsy vaue then it will return the last truthy value.
+
+const firstName = "Arbaz";
+const lastName = "Haider";
+const nickNaam = "Arbu";
+let officialNaam;
+const petName = null;
+
+const userNaam = firstName && lastName;
+console.log(`Name - ${userNaam}`);
+
+const userNaam1 = firstName && nickNaam;
+console.log(`Name - ${userNaam1}`);
+
+const userNaam2 = firstName && officialNaam;
+console.log(`Name - ${userNaam2}`);
+
+const userNaam3 = firstName && petName;
+console.log(`Name - ${userNaam3}`);
+
+const userNaam4 = firstName && lastName && "Siwan"; //Short circuiting
+console.log(`Name - ${userNaam4}`);
+
 /** OR || */
 
 if (background === "PCM") {
@@ -86,6 +115,68 @@ if (background === "PCM") {
 } else {
   console.log("Invalid entry. Please enter either 'PCM' or 'PCB'.");
 }
+
+//Truthy & Falsy
+
+//In expression OR will search for first truthy value if it gets first truthy value it will return that value to the variable or it will log that value.
+//If it doesn't find the first truthy value if all the values are falsy values it will return the last falsy values.
+
+//Example:
+//1.)
+const fName0 = "Arbaz";
+const lName0 = "Haider";
+const nickName0 = "Arbu";
+const userName0 = fName0 || lName0 || nickName0;
+console.log(`Name - ${userName0}`);
+
+//2.)
+const fName1 = "";
+const lName1 = "";
+const nickName1 = "Arbu";
+const userName1 = fName1 || lName1 || nickName1;
+console.log(`Name - ${userName1}`);
+
+//3.)
+const fName2 = null;
+const lName2 = undefined;
+const nickName2 = "";
+const userName2 = fName2 || lName2 || nickName2;
+console.log(`Name - ${userName2}`);
+
+//4.)
+const fName3 = "";
+const lName3 = undefined;
+const nickName3 = null;
+const userName3 = fName3 || lName3 || nickName3;
+console.log(`Name - ${userName3}`);
+
+//5.)
+const fName4 = "";
+const lName4 = null;
+const nickName4 = undefined;
+const userName4 = fName4 || lName4 || nickName4;
+console.log(`Name - ${userName4}`);
+
+//6.)
+const fName5 = null;
+const lName5 = "";
+const nickName5 = undefined;
+const userName5 = fName5 || lName5 || nickName5 || "Siwan";
+console.log(`Name - ${userName5}`);
+
+let a = 12;
+let b;
+let c = 3;
+let d = null;
+let e = "";
+
+console.log(a + b); //NaN
+console.log(a + (b || 0)); //12
+console.log(a + d); //12
+console.log(a + (d || 0)); //12
+console.log(a + e); //12
+console.log(a + (e || 0)); //12
+console.log(a + (c || 0)); //15
 
 /** NOT ! */
 
