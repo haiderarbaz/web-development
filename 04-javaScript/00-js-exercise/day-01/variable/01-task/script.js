883614e9dfeb0eca96c82782a96872fa0a46d5aa
haiderarbaz <admin@arbazhaider.com>
2025-06-21 15:12:20 +0000

    Add day-01/ code file to codebase

---
+++ 04-javaScript/00-js-exercise/day-01/variable/01-task/script.js
@@ -0,0 +1,18 @@
+// Task-01: Working with variables
+
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+let userName = "John";
+let admin = userName;
+
+// you can declare both variables at once like below
+// let userName, admin
+
+// after declaring you can assign value into them like below
+// userName = "John";
+// admin = name;
+
+alert(admin);
